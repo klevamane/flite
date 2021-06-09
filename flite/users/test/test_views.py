692225073626289c85316e95156e057dae8de82c
Onengiye Onengiye <klevamane@gmail.com>
2021-06-09 02:37:39 +0100

    tests: write tests

--- flite/users/test/test_views.py
+++ flite/users/test/test_views.py
@@ -6,7 +6,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from faker import Faker
 from ..models import User,UserProfile,Referral
-from .factories import UserFactory
+from .factories import UserFactory, BalanceFactory, DepositFactory
 
 fake = Faker()
 
@@ -80,18 +80,78 @@ class TestUserDetailTestCase(APITestCase):
 
 class TestTransactions(APITestCase):
 
+    def setUp(self):
+        self.user = UserFactory()
+        self.user2 = UserFactory()
+        self.url = reverse('user-detail', kwargs={'pk': self.user.pk})
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.user.auth_token}')
+        self.deposit_url = reverse('deposit-url', kwargs={'user_id': self.user.pk})
+        self.withdrawal_url = reverse('withdrawal-url', kwargs={'user_id': self.user.pk})
+        self.transactions_url = reverse("user-transactions", kwargs={'account_id': self.user.pk})
+        self.p2p_transfer_url = reverse('p2p-transfer-url', kwargs={'sender_account_id': self.user.pk, 'recipient_account_id': self.user2.pk})
+        # self.transaction_url = reverse("user-transaction", kwargs={'transaction_id': self.user.pk})
+
+        DepositFactory.create(owner=self.user)
+        self.transaction_url = reverse("user-transaction", kwargs={'transaction_id': self.user.transaction.first().id})
+
     def test_user_can_make_a_deposit(self):
-        assert False
+        payload = {"amount": 100}
+        response = self.client.post(self.deposit_url, payload)
+        eq_(response.status_code, status.HTTP_201_CREATED)
+        eq_(self.user.balance.available_balance, 0)
+        # asset after deposit
+        self.user.balance.refresh_from_db()
+        eq_(self.user.balance.available_balance, 100)
 
     def test_user_can_make_a_withdrawal(self):
-        assert False
+        self.user.balance.book_balance = 100
+        self.user.balance.available_balance = 100
+        self.user.balance.save()
+
+        payload = {"amount": 10}
+        response = self.client.post(self.withdrawal_url, payload)
+        eq_(response.status_code, status.HTTP_201_CREATED)
+        eq_(self.user.balance.available_balance, 100)
+        # asset after deposit
+        self.user.balance.refresh_from_db()
+        eq_(self.user.balance.available_balance, 90)
+        eq_(self.user.balance.book_balance, 90)
+
 
     def test_user_can_make_a_p2p_transfer(self):
-        assert False
+        self.user.balance.book_balance = 100
+        self.user.balance.available_balance = 100
+        self.user.balance.save()
+
+        payload = {"amount": 70}
+        # assert sender before transfer
+        eq_(self.user.balance.available_balance, 100)
+        response = self.client.post(self.p2p_transfer_url, payload)
+        eq_(response.status_code, status.HTTP_201_CREATED)
+
+        # assert sender after transfer
+        self.user.balance.refresh_from_db()
+        eq_(self.user.balance.available_balance, 30)
+        eq_(self.user.balance.book_balance, 30)
+
+        # assert recipient after transfer
+        self.user2.balance.refresh_from_db()
+        eq_(self.user2.balance.book_balance, 70)
+        eq_(self.user2.balance.available_balance, 70)
+
 
     def test_user_can_fetch_all_transactions(self):
-        assert False
+
+        response = self.client.get(self.transactions_url)
+        # assert after fetching data
+        eq_(response.status_code, status.HTTP_200_OK)
+        eq_(self.user.transaction.count(), 1)
 
     def test_user_can_fetch_a_single_transaction(self):
-        assert False
+        # a deposit transaction by the current user
+        # already exist from setup
+        response = self.client.get(self.transaction_url)
+        eq_(response.status_code, status.HTTP_200_OK)
+
+
 
